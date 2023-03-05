82d03bd318d23350a5696ef56ad9cc829bde786f
WeiMoKaungLong <infinity19971023@gmail.com>
2023-03-05 18:05:03 +0800

    Initial Commit

---
+++ my_ios_RedSo(new)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my_ios_RedSo(new)
+//
+//  Created by 危末狂龍 on 2023/3/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
